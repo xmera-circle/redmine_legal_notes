406d06e39d250bd5e81e831e3029051d48fa5897
Liane Hampe <liaham@xmera.de>
2020-10-28 14:41:11 +0100

    Initial commit of plugin skeleton

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_legal_notes do
+  name 'Redmine Legal Notes Plugin'
+  author 'Liane Hampe, xmera'
+  description 'Dedicated pages for data privacy policy and legal notice'
+  version '0.0.1'
+  url 'https://circle.xmera.de/projects/redmine-legal-notes'
+  author_url 'http://xmera.de'
+end
