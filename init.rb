8d18f200ec90c01dc0e13c2c7a74665bee7d69b6
Liane Hampe <liaham@xmera.de>
2020-10-29 11:31:13 +0100

    Add Redmine version requirement in init.rb

--- init.rb
+++ init.rb
@@ -5,4 +5,6 @@ Redmine::Plugin.register :redmine_legal_notes do
   version '0.0.1'
   url 'https://circle.xmera.de/projects/redmine-legal-notes'
   author_url 'http://xmera.de'
+
+  requires_redmine version_or_higher: '4.0.0'
 end
